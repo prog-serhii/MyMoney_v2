a1f4ee02c0d7f97b4ab7e012fd9e3a46a3d15093
Serhii Kazmiruk <kazmiruk.sergii@gmail.com>
2020-11-09 15:06:07 +0100

    Add currency validation

--- backend/apps/wallet/serializers.py
+++ backend/apps/wallet/serializers.py
@@ -1,17 +1,37 @@
+from moneyed.classes import get_currency, CurrencyDoesNotExist
 from djmoney.contrib.django_rest_framework import MoneyField
 from rest_framework import serializers
 
 from .models import Wallet
 
 
+class CurrencyCodeValidator:
+    """
+    Validator to check if this currency exists
+    """
+
+    def __call__(self, value):
+        try:
+            # try to find currency with this code
+            get_currency(code=value)
+        except CurrencyDoesNotExist as e:
+            raise serializers.ValidationError(str(e))
+
+
 class WalletSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    # ДОДАТИ ДОЗВІЛ ТІЛЬКИ ДЛЯ АВТОРИЗОВАНИХ
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault())
+    start_balance_currency = serializers.CharField(
+        required=True, write_only=True, validators=[CurrencyCodeValidator()])
 
     class Meta:
         model = Wallet
         exclude = ['id', 'logo']
-        extra_kwargs = {
-            'start_balance': {'write_only': True},
-        }
 
-    # ДОДАТИ ВАЛІДАЦІЮ НА start_balance_currency
+        extra_kwargs = {
+            'start_balance':
+            {
+                'write_only': True
+            },
+        }
