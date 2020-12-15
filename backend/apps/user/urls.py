8737584c41f9765d84d850bf1deeb0d585560d70
Serhii Kazmiruk <kazmiruk.sergii@gmail.com>
2020-12-15 19:34:39 +0100

    Add an endpoint to represent all available currencies

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from .views import UserProfileView
+from .views import UserProfileView, AllCurrencuesView
+
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('user/', UserProfileView.as_view(), name='api-current-user')
+    path('user/', UserProfileView.as_view(), name='api-current-user'),
+    path('currencies/', AllCurrencuesView.as_view(), name='api-all-available-currencies')
 ]
--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from djmoney.settings import CURRENCY_CHOICES
 
 from .serializers import UserSerializer
 
@@ -10,3 +14,13 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         user = self.request.user
         return user
+
+
+class AllCurrencuesView(APIView):
+    """
+    A view that returns list of available currencies.
+    """
+
+    def get(self, request, format=None):
+        content = [{'code': code, 'name': name} for code, name in CURRENCY_CHOICES]
+        return Response(content)
